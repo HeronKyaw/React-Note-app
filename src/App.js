53a52c64c12aa72d5cabe49ff16e4a293768c9a5
Wunna Kyaw <heronskyaw@gmail.com>
2023-01-19 00:36:27 +0630

    finish Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Main from './Components/Main';
+import SideBar from './Components/SideBar';
+import uuid from 'react-uuid';
 
 function App() {
+  const [notes, setNotes] = useState(localStorage.notes ? JSON.parse(localStorage.notes) : []);
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  },[notes]);
+
+  const onAddNote = () =>{
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now()
+    };
+    
+    setNotes([newNote, ...notes]);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNoteArray = notes.map((note)=> {
+      if(note.id === activeNote) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNoteArray);
+  }
+  
+  const onDeleteNote = (idToDelete) => {
+    if (window.confirm('Are you sure to delet this note?')) {
+      setNotes(notes.filter((note) => note.id !== idToDelete));
+    }
+  }
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar 
+        notes={notes} 
+        onAddNote={onAddNote} 
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main 
+        activeNote={getActiveNote()}
+        onUpdateNote={onUpdateNote}
+      />
     </div>
   );
 }
